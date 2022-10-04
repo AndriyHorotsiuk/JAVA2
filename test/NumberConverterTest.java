2044f39734998ba904cf0888351549b1e71b27bb
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-10-04 18:11:13 +0300

    add classTest for all class of Namber converter

--- test/NumberConverterTest.java
+++ test/NumberConverterTest.java
@@ -1,2 +1,14 @@
 public class NumberConverterTest {
+    public static  inttostrTest(int number) {
+
+    }
+    public static strtointTest(String number) {
+
+    }
+    public static dbltostrTest(double number){
+
+    }
+    public static strtodblTest(String number){
+        
+    }
 }
