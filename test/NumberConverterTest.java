041b2858b64a5a25d8ae72dcf4f90df581a59bfa
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-10-04 21:44:22 +0300

    write inttostrTest

--- test/NumberConverterTest.java
+++ test/NumberConverterTest.java
@@ -1,14 +1,31 @@
 public class NumberConverterTest {
-    public static  inttostrTest(int number) {
+
+    public static void main(String[] args) {
+        NumberConverterTest.inttostrTest();
+
 
     }
-    public static strtointTest(String number) {
+
+    public static void inttostrTest() {
+
+        int number = 15;
+        String result = NumberConverter.inttostr(number);
+        if ("15".equals(result)) {
+            System.out.println("test is successful");
+        } else {
+            System.out.println("fail");
+        }
+    }
+
+    public void strtointTest(String number) {
 
     }
-    public static dbltostrTest(double number){
+
+    public void dbltostrTest(double number) {
 
     }
-    public static strtodblTest(String number){
-        
+
+    public void strtodblTest(String number) {
+
     }
 }
