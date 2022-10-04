d3a40775ac752f8ea629afb2b7c31df90fd00f56
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-10-04 12:35:32 +0300

    created directory Test and class NumberConverterTest

--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,13 +1,9 @@
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-import java.util.Scanner;
 
 public class NumberConverter {
 
 
     public static String inttostr(int number) {
-        //System.out.println("Input namber for converter to String type");
-        //Scanner scan =new Scanner(System.in);
-        //int namber=scan.nextInt();
+
 
         String[] symbol = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
         String str = "";
@@ -35,9 +31,6 @@ public class NumberConverter {
     }
 
     public static int strtoint(String number) {
-        // System.out.println("Input namber for converter to int type");
-        //  Scanner scan = new Scanner(System.in);
-        // String namber = scan.nextLine();
 
         String symbol = "0123456789";
         int intnum = 0;
@@ -100,7 +93,7 @@ public class NumberConverter {
             dstr = "-" + intdbl.substring(0, intdbl.length() - i) + "," + intdbl.substring(intdbl.length() - i);
 
         }
-        // System.out.println(dstr);
+
         return dstr;
     }
 
@@ -114,45 +107,6 @@ public class NumberConverter {
         return (a);
     }
 
-    public static void test() {
-        System.out.println("1-inttostr 2-strtoint 3-strtodbl");
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        if (a == 1) {
-            System.out.println("Enter an integer umber to convert to a string");
-            int intnum = scan.nextInt();
-            System.out.println(NumberConverter.inttostr(intnum));
-        }
-        if (a == 2) {
-            System.out.println("Enter a string  number to convert to an integer");
-            String strnum1 = scan.nextLine();
-            System.out.println(NumberConverter.strtoint(strnum1));
-        } else {
-            System.out.println("Enter a string  number to convert to an double");
-            String strnum = scan.nextLine();
-            System.out.println(NumberConverter.strtodbl(strnum));
-
-        }
-    /* switch (a){
-         case 1:
-             System.out.println("Enter an integer umber to convert to a string");
-             int intnum = scan.nextInt();
-             System.out.println(NamberConverter.inttostr(intnum));
-               break;
-         case 2:
-             System.out.println("Enter a string  number to convert to an integer");
-             String strnum1=scan.nextLine();
-             System.out.println(NamberConverter.strtoint(strnum1));
-              break;
-         case 3:
-             System.out.println("Enter a string  number to convert to an double");
-             String strnum=scan.nextLine();
-             System.out.println(NamberConverter.strtodbl(strnum));
-               break;
-               System.out.println("Enter a string  number to convert to an double");
-
-
-     }*/
 
 
     }
