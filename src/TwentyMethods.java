f595c66605a2396455e486ed9661c3e9404cad90
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-10-27 18:11:14 +0300

    Add 20-th method

--- src/TwentyMethods.java
+++ src/TwentyMethods.java
@@ -182,5 +182,17 @@ public class TwentyMethods {
         return arrSortDescending(arr);
     }
 
+    //Method 20
+    public static boolean RepeateValueInArr(byte[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
 
 }
