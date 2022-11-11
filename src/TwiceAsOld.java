fc7006324de6f14adce4252c9704ceb6a518167d
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-11-11 17:28:19 +0200

    optimize

--- src/TwiceAsOld.java
+++ src/TwiceAsOld.java
@@ -1,14 +1,6 @@
 public class TwiceAsOld {
     public static int twiseFatherOldAsSon(int ageSon, int ageFather) {
-        int years = 0;
-        if (2 * ageSon - ageFather > 0) {
-            years = 2 * ageSon - ageFather;
-
-
-        } else {
-            years = ageFather - 2 * ageSon;
-
-        }
+        int   years = ageFather-2 * ageSon;
         return years;
     }
 }
