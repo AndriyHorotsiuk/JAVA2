29f7654b500380dc718be5f175fc6f63684aad0d
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-11-11 16:55:49 +0200

    rewrite without cycles

--- src/TwiceAsOld.java
+++ src/TwiceAsOld.java
@@ -1,21 +1,14 @@
 public class TwiceAsOld {
-    public static void TwiseFatherOldAsSon(int ageFather, int ageSon) {
-        int i = 0;
+    public static void twiseFatherOldAsSon(int ageSon, int ageFather) {
+        int years = 0;
         if (2 * ageSon - ageFather > 0) {
-            while (ageFather != 2 * ageSon) {
-                ageFather--;
-                ageSon--;
-                i++;
-            }
-            System.out.println(i + " years ago the father was twice as old as his son");
+            years=2*ageSon-ageFather;
+
+            System.out.println(years + " years ago the father was twice as old as his son");
 
         } else {
-            while (ageFather != 2 * ageSon) {
-                ageFather++;
-                ageSon++;
-                i++;
-            }
-            System.out.println("After " + i + " years the father will be twice as old as his son");
+            years=ageFather-2*ageSon;
+            System.out.println("After " + years + " years the father will be twice as old as his son");
         }
     }
 }
