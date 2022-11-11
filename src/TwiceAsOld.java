9049521c1be06fed44aff60e70d614f1fdfc88b6
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-11-11 17:15:55 +0200

    return years

--- src/TwiceAsOld.java
+++ src/TwiceAsOld.java
@@ -1,14 +1,14 @@
 public class TwiceAsOld {
-    public static void twiseFatherOldAsSon(int ageSon, int ageFather) {
+    public static int twiseFatherOldAsSon(int ageSon, int ageFather) {
         int years = 0;
         if (2 * ageSon - ageFather > 0) {
-            years=2*ageSon-ageFather;
+            years = 2 * ageSon - ageFather;
 
-            System.out.println(years + " years ago the father was twice as old as his son");
 
         } else {
-            years=ageFather-2*ageSon;
-            System.out.println("After " + years + " years the father will be twice as old as his son");
+            years = ageFather - 2 * ageSon;
+
         }
+        return years;
     }
 }
