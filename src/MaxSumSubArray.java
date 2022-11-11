6324ab58d193fa8ac3a618069c0237f878476237
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-11-11 18:12:41 +0200

    changed the algorithm

--- src/MaxSumSubArray.java
+++ src/MaxSumSubArray.java
@@ -1,18 +1,19 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class MaxSumSubArray {
     public static int maxSumSubArr(int[] arr) {
-        ArrayList<Integer> sumSubArray = new ArrayList<>();
 
-        for (int i = 1; i < arr.length + 1; i++) {
-            for (int j = 0; j < arr.length - i + 1; j++) {
-                int sumSub = Arrays.stream(Arrays.copyOfRange(arr, j, j + i)).sum();
-                sumSubArray.add(sumSub);
-            }
+        int maxSum = 0;
+        int subSum = 0;
+        for (int ellementarr: arr) {
+            subSum += ellementarr;
+            maxSum = Math.max(maxSum, subSum);
+            if (subSum < 0) subSum = 0;
         }
-
-        return (Collections.max(sumSubArray) > 0) ? Collections.max(sumSubArray): 0;
+        return maxSum;
     }
+
+
 }
+
+
+
+
