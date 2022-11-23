50c1d51ca4aac01bd5cd8f6d6e7281a200a0a3a8
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-11-23 17:20:15 +0200

    add test Panagram

--- src/Panagram.java
+++ src/Panagram.java
@@ -2,12 +2,12 @@
 
 public class Panagram {
     public static boolean isPanagram(String str) {
-       str = str.toUpperCase();
+        str = str.toUpperCase();
 
         char[] alphabet = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         for (char ell : alphabet) {
             if (str.indexOf(ell) == -1) {
-        return false;
+                return false;
             }
         }
 
