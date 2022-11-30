9e63c4c4d35626b1a6b72d36106abb1062b46447
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-11-30 18:17:43 +0200

    ArrayList

---
+++ src/Array_List.java
@@ -0,0 +1,30 @@
+public class Array_List<T> {
+    /* public Array_List (int sizeArrayList){
+
+     }*/
+    private int sizeArrayList = 10;
+
+    private Object[] array_List = new Object[sizeArrayList];
+
+    int lengthPaddingArr = 0;
+
+    public Object[] arrcopy(Object[] arrFrom, Object[] arrTo) {
+        for (int i = 0; i < arrFrom.length - 1; i++) {
+            arrTo[i] = arrFrom[i];
+        }
+        return arrTo;
+    }
+
+    public void add(T ell) {
+        if (lengthPaddingArr > (array_List.length - 1)) {
+            Object[] temporaryArray = new Object[sizeArrayList];
+            temporaryArray = arrcopy(array_List, temporaryArray);
+            sizeArrayList = 2 * sizeArrayList;
+            Object[] array_List = new Object[sizeArrayList];
+            array_List = arrcopy(temporaryArray, array_List);
+        }
+        array_List[lengthPaddingArr] = ell;
+        lengthPaddingArr++;
+
+    }
+}
