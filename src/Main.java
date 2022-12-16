4267abd1080057a80b1ff88f8abb238a29087657
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2022-12-16 15:57:24 +0200

    all method v 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        MyArrayList<String> str = new MyArrayList<String>();
-      str.add("dfgg");
-       /* str.add("dfgg");
-        str.add("q");
-        str.add("dfgg");
-        str.add("dfgg");
-        str.add("q");
-      str.remove(0);
-        str.remove(0);
-        str.remove(0);
-        str.remove(0);
-*/
 
-        System.out.println(str.size());
     }
 
 }
