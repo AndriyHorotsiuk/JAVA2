f2c72326bcd9b4ee7fc0b47db337eaa29a6e4acb
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2023-01-17 16:49:22 +0200

    SwitchPairs

--- src/SwitchPairs.java
+++ src/SwitchPairs.java
@@ -6,7 +6,6 @@ public class SwitchPairs {
     public static ArrayList<String> SwitchPairsArrayList() {
         ArrayList<String> arrayList = new ArrayList<String>();
         for (int i = 0; i < (arrayList.size() - arrayList.size() % 2); i++) {
-
             arrayList.add(i, arrayList.get(i + 1));
             arrayList.remove(i + 2);
             i++;
@@ -14,5 +13,4 @@ public class SwitchPairs {
         return arrayList;
 
     }
-
 }
