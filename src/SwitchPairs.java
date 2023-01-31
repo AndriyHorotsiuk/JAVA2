555a06e4a756b278ea3876377f65be79cd3b5e23
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2023-01-31 13:53:33 +0200

    Made changes

--- src/SwitchPairs.java
+++ src/SwitchPairs.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 
 public class SwitchPairs {
 
-    public static ArrayList<String> SwitchPairsArrayList() {
-        ArrayList<String> arrayList = new ArrayList<String>();
+    public static ArrayList<String> switchPairsArrayList(ArrayList<String> arrayList) {
         for (int i = 0; i < (arrayList.size() - arrayList.size() % 2); i++) {
             arrayList.add(i, arrayList.get(i + 1));
             arrayList.remove(i + 2);
