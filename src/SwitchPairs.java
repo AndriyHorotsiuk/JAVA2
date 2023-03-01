8c5ab0941e03c5d38df019f6d09649a17104658f
AndriyHorotsiuk <AndriyHorotsiuk@gmail.com>
2023-03-01 13:00:29 +0200

    rewrite by set

--- src/SwitchPairs.java
+++ src/SwitchPairs.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 public class SwitchPairs {
 
     public static ArrayList<String> switchPairsArrayList(ArrayList<String> arrayList) {
-        for (int i = 0; i < (arrayList.size() - arrayList.size() % 2); i++) {
-            arrayList.add(i, arrayList.get(i + 1));
-            arrayList.remove(i + 2);
+        String itemExchange;
+        for (int i = 0; i < (arrayList.size() % 2); i++) {
+            itemExchange =arrayList.get(i);
+            arrayList.set(i,arrayList.get(i+1));
+            arrayList.set(i+1, itemExchange);
             i++;
         }
 
